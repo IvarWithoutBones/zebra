7e72cbd9e8cb88deb8834a15135a61add583c050
Ivar Scholten <ivar.scholten@protonmail.com>
2023-02-06 17:49:07 +0100

    handle panics

--- kernel/src/asm/entry.s
+++ kernel/src/asm/entry.s
@@ -1,23 +1,21 @@
-# Disable generation of compressed instructions.
+# Disable generation of compressed instructions
 .option norvc
 
 .section .text.init
-
 .global _start
 _start:
-	# Disable linker instruction relaxation for the `la` instruction below,
-	# this disallows the assembler from assuming that `gp` is already initialized.
+	# Stop the assembler from assuming that `gp` is already initialized
     .option push
     .option norelax
 	la gp, _global_pointer
     .option pop
 
-	# Should be zero, but let's make sure
+	# Should be zero but let's make sure
 	csrw satp, zero
 
 	# Any hardware threads (hart) other than hart 0 should park
 	csrr t0, mhartid
-	bnez t0, park
+	bnez t0, park_hart
 
 	# Clear the BSS section
 	la a0, _bss_start
@@ -34,8 +32,8 @@ clear_bss_loop:
 	li t0, 0b11 << 11 | (1 << 13)
 	csrw mstatus, t0
 
-	# Machine's exception program counter (MEPC) is set to `kernel_main`, our Rust entry point.
-    # We will jump to this when we return.
+	# Machine's exception program counter (MEPC) is set to our Rust entry point,
+    # we will jump to this when we call `mret` below.
 	la t1, kernel_main
 	csrw mepc, t1
 
@@ -43,17 +41,38 @@ clear_bss_loop:
 	la t2, trap_vector
 	csrw mtvec, t2
 
-	# Place to continue execution after `mepc` returns.
-	la ra, park
+    call print_str # TODO: remove
+
+	# Place to continue execution after `mepc` returns, this should never be reached.
+	la ra, park_hart
 
 	# Update mstatus and jump to `mepc`
 	mret
 
-park:
-	wfi
-	j park
-
 # This will be called upon interrupts, no-op for now
 .global trap_vector
 trap_vector:
+    mret
+
+# Random stuff to get familiar with ASM
+
+.align 3
+test: .ascii "this is being printed from ASM\n\0"
+
+print_str:
+    la t0, test
+    li t1, 0x10000000 # UART
+
+    lbu t2, (t0)
+print_str_loop:
+    sb t2, (t1) # Write current character to UART
+    addi t0, t0, 1 # Increment pointer to next character
+    lbu t2, (t0) # Load next character
+    bnez t2, print_str_loop # Repeat unless we hit a null
+
+    mret
+
+print_num_rust:
+    li a0, 123456
+    call print_num
     mret
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,35 +1,88 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 // Tests dont do anything for now, but this shuts up rust-analyzer
 #![feature(custom_test_frameworks)]
 #![test_runner(test_runner)]
-
-core::arch::global_asm!(include_str!("./asm/entry.s"));
-
 #[cfg(test)]
 fn test_runner(_tests: &[&dyn Fn()]) {}
 
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+use core::{
+    arch::{asm, global_asm},
+    panic::PanicInfo,
+};
+
+global_asm!(include_str!("./asm/entry.s"));
+
+const UART: *mut u8 = 0x1000_0000 as _;
+
+fn print(s: &str) {
+    unsafe {
+        for b in s.bytes() {
+            UART.write_volatile(b);
+        }
+    }
 }
 
-extern "C" {
-    fn park();
+fn println(s: &str) {
+    print(s);
+    print("\n");
 }
 
-unsafe fn println(s: &str) {
-    const UART: *mut u8 = 0x1000_0000 as _;
-    for c in s.bytes() {
+#[no_mangle]
+extern "C" fn print_num(num: usize) {
+    const RADIX: usize = 10;
+    let tens = num / RADIX;
+    if tens > 0 {
+        print_num(tens);
+    }
+
+    let c = (num % RADIX) as u8 + if num % RADIX < 10 { b'0' } else { b'a' - 10 };
+    unsafe {
         UART.write_volatile(c);
     }
-    UART.write_volatile(b'\n');
+}
+
+#[no_mangle]
+unsafe extern "C" fn park_hart() -> ! {
+    unsafe {
+        asm!("wfi", "j {park}", park = sym park_hart, options(noreturn));
+    }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(s) = info.payload().downcast_ref::<&str>() {
+        print("panicked: ");
+        print(s);
+    } else {
+        if let Some(msg) = info.message() {
+            print("panicked at '");
+            if let Some(str) = msg.as_str() {
+                print(str);
+            } else {
+                print("unknown");
+            }
+            print("', ");
+        }
+
+        if let Some(loc) = info.location() {
+            print(loc.file());
+            print(":");
+            print_num(loc.line() as _);
+            print(":");
+            print_num(loc.column() as _);
+        }
+    }
+    print("\n");
+    unsafe { park_hart() }
 }
 
 #[no_mangle]
 extern "C" fn kernel_main() {
+    println("kernel_main() called, we have reached Rust!");
     unsafe {
-        println("kernel_main() called, we have reached Rust!");
-        core::arch::asm!("li t4, 0xFEEDFACECAFEBEEF", "j {park}", park = sym park);
+        asm!("li t4, 0xFEEDFACECAFEBEEF");
     }
+    panic!("this should panic");
 }
